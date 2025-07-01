33d776fa1b4ff19fb20e4a5ebc5db769f84cc201
Jorge Blas <nave3.borreguito@gmail.com>
2025-07-01 17:57:23 -0300

    Agrego service worker de Firebase Messaging

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,1 +1,24 @@
-// Firebase Service Worker (simulado)
+// firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey:            "AIzaSyB4Pfj-ETDoShEomPgJLwhAieqxX5jxNVw",
+  authDomain:        "borreguito-push.firebaseapp.com",
+  projectId:         "borreguito-push",
+  storageBucket:     "borreguito-push.appspot.com",
+  messagingSenderId: "1050243032967",
+  appId:             "1:1050243032967:web:778188baaa30fbd1fcd522",
+  vapidKey:          "BMswRbiMiMM4fR6CKmDvPMi12Fh56coE-Z_aOJYwZQeIyybyYFqaywcv6dpcCONTH07C_CcljvvjiPNmM26yB0c"
+});
+
+const messaging = firebase.messaging();
+
+// Opcional: mostrar notificaciones personalizadas en segundo plano
+messaging.onBackgroundMessage(function(payload) {
+  const { title, body, icon } = payload.notification;
+  self.registration.showNotification(title, {
+    body: body,
+    icon: icon
+  });
+});
